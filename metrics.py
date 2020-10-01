a97eaf3af19f016bbf820998dcb2314dfdbdd41d
siitake <>
2020-10-01 11:35:56 +0900

    add: snr

--- metrics.py
+++ metrics.py
@@ -2,5 +2,33 @@
 センサデータに関する評価関数
 
 追加予定
-MSE, SNR,..
+SNR,..
 """
+
+import typing
+
+import numpy as np
+
+
+def snr(dst:np.ndarray, src:np.ndarray, axis:typing.Optional[int]=None) -> typing.Union[float, np.ndarray]:
+    """to calc Signal to Noise Ratio.
+
+    Parameters
+    ----------
+    dst: np.ndarray
+        clean data
+
+    src: np.ndarray
+        with noise
+
+    axis: Optional[int], default=None
+        mean axis
+
+    Returns
+    -------
+    Union[float, np.ndarray]:
+        [dB]
+    """
+    noise_mse = (np.square(dst - src)).mean(axis=axis)
+    signal_ms = (np.square(dst)).mean(axis=axis)
+    return 20 * np.log10((signal_ms / noise_mse))
