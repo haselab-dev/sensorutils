70c6ce03ec945ff53b7041c8d9030cc2ef51b0d7
foo2810 <ntshp.ak@gmail.com>
2021-12-06 17:19:19 +0900

    add DSADS loader class and test code for the loader

--- src/sensorutils/datasets/dsads.py
+++ src/sensorutils/datasets/dsads.py
@@ -5,12 +5,12 @@ https://archive.ics.uci.edu/ml/datasets/daily+and+sports+activities
 
 import itertools
 from pathlib import Path
-from typing import List, Tuple, Union
+from typing import List, Optional, Tuple, Union
 
 import numpy as np
 import pandas as pd
 
-from .base import check_path
+from .base import BaseDataset, check_path
 
 
 __all__ = ['load', 'load_raw']
@@ -50,6 +50,157 @@ ACTIVITIES_NAMES = (
 ACTIVITIES = dict(
     zip(range(18+1), ACTIVITIES_NAMES)
 )
+
+
+class DSADS(BaseDataset):
+    """
+    DSADSデータセットに記録されているセンサデータとメタデータを読み込む．
+
+    Parameters
+    ----------
+    path: Path
+        DSADSデータセットのパス(path/to/dataset/data").
+    
+    Attributes
+    ----------
+    supported_x_labels: List[str]
+        ターゲット以外のすべてのラベルのリスト
+
+    supported_y_labels: List[str]
+        ターゲットラベルのリスト
+    """
+
+    supported_x_labels = tuple(set(COLUMNS) - set(['activity', 'subject']))
+    supported_y_labels = ('activity', 'subject')
+
+    def __init__(self, path:Path):
+        super().__init__(path)
+        self.data_cache = None
+    
+    def _load(self):
+        if self.data_cache is None:
+            segments = load_raw(self.path)
+            self.data_cache = segments
+        else:
+            segments = self.data_cache
+        return segments
+    
+    def load(self,
+        x_labels:Optional[list]=None, y_labels:Optional[list]=None,
+        subjects:Optional[list]=None) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        DSASSデータセットを読み込み，sliding-window処理を行ったデータを返す．
+
+        Parameters
+        ----------
+        x_labels: Optional[list]
+            入力(従属変数)のラベルリスト(ラベル名は元データセットに準拠)．ここで指定したラベルのデータが入力として取り出される．
+
+        y_labels: Optional[list]
+            ターゲットのラベルリスト(仕様はx_labelsと同様)
+
+        subjects:
+            ロードする被験者を指定する．指定されない場合はすべての被験者のデータを返す．
+            被験者は計9名おり，それぞれにIDが割り当てられている．
+            
+            被験者ID: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        
+        Returns
+        -------
+        (x_frames, y_frames): Tuple[np.ndarray, np.ndarray]
+            sliding-windowで切り出した入力とターゲットのフレームリスト
+
+            x_framesは3次元配列で構造は大まかに(Batch, Channels, Frame)のようになっている．
+            Channelsはx_labelsで指定したものが格納される．
+
+            y_framesは2次元配列で構造は大まかに(Batch, Labels)のようになっている．
+            Labelsはy_labelsで指定したものが格納される．
+
+            y_framesはデータセット内の値をそのまま返すため，分類で用いる際はラベルの再割り当てが必要となることに注意する．
+        
+        See Also
+        --------
+        一度loadメソッドを読みだすと内部にキャッシュを作成するため2回目以降のloadメソッドの読みだしは比較的速くなる．
+
+        x_labelsやy_labelsに指定できるラベルは次の通りである．
+
+        'T_xacc', 'T_yacc', 'T_zacc',
+        'T_xgyro', 'T_ygyro', 'T_zgyro',
+        'T_xmag', 'T_ymag', 'T_zmag',
+        'RA_xacc', 'RA_yacc', 'RA_zacc',
+        'RA_xgyro', 'RA_ygyro', 'RA_zgyro',
+        'RA_xmag', 'RA_ymag', 'RA_zmag',
+        'LA_xacc', 'LA_yacc', 'LA_zacc',
+        'LA_xgyro', 'LA_ygyro', 'LA_zgyro',
+        'LA_xmag', 'LA_ymag', 'LA_zmag',
+        'RL_xacc', 'RL_yacc', 'RL_zacc',
+        'RL_xgyro', 'RL_ygyro', 'RL_zgyro',
+        'RL_xmag', 'RL_ymag', 'RL_zmag',
+        'LL_xacc', 'LL_yacc', 'LL_zacc',
+        'LL_xgyro', 'LL_ygyro', 'LL_zgyro',
+        'LL_xmag', 'LL_ymag', 'LL_zmag'
+        'activity', 'subject'
+
+        Examples
+        --------
+        >>> dsads_path = Path('path/to/dataset/data/')
+        >>> dsads = DSASS(dsads_path)
+        >>>
+        >>> x_labels = ['T_accx', 'T_accy', 'T_accz']
+        >>> y_labels = ['activity', 'subject']
+        >>> x, y = dsads.load(x_labels=xlabels, y_labels=y_labels)
+        >>> print(f'x: {x.shape}, y: {y.shape}')
+        >>>
+        >>> # > x: (?, 3, 125), y: (?, 1)
+        """
+
+        global SUBJECTS
+        if subjects is not None:
+            if not (set(subjects) <= set(SUBJECTS)):
+                raise ValueError('detect unknown person, {}'.format(subjects))
+        
+        if x_labels is None:
+            x_labels = list(self.supported_x_labels)
+        if y_labels is None:
+            y_labels = list(self.supported_y_labels)
+        if not(set(x_labels) <= set(self.supported_x_labels)):
+            raise ValueError('unsupported x labels is included: {}'.format(
+                tuple(set(x_labels) - set(self.supported_x_labels).intersection(set(x_labels)))
+            ))
+        if not(set(y_labels) <= set(self.supported_y_labels)):
+            raise ValueError('unsupported y labels is included: {}'.format(
+                tuple(set(y_labels) - set(self.supported_y_labels).intersection(set(y_labels)))
+            ))
+
+        segments = self._load()
+        segments = [seg[x_labels+y_labels+['subject']].to_numpy() for seg in segments]
+        frames = np.stack(segments)
+        assert frames.shape[-1] == len(x_labels) + len(y_labels) + 1, 'Extracted data shape does not match with the number of total labels'
+        # x_labelsでサポートされているラベルはすべてfloat64で対応
+        # y_labelsでサポートされているラベルはすべてint8で対応可能
+        x_frames = np.float64(frames[:, :, :len(x_labels)]).transpose(0, 2, 1)
+        y_frames = np.int8(frames[:, 0, len(x_labels):])
+
+        # subject filtering
+        if subjects is not None:
+            subject_labels = y_frames[:, -1]
+            x_frames, y_frames = self._filter_by_subject(x_frames, y_frames, subject_labels, subjects)
+        
+        # remove tail columns(subject)
+        y_frames = y_frames[:, :-1]
+
+        return x_frames, y_frames
+
+    def _filter_by_subject(self, x_frames, y_frames, subject_labels, subjects):
+        p_flags = np.zeros(len(x_frames), dtype=bool)
+        for s in subjects:
+            p_flags = np.logical_or(p_flags, subject_labels == s)
+        flags = p_flags
+
+        # filter
+        x_frames, y_frames = x_frames[flags], y_frames[flags]
+
+        return x_frames, y_frames
 
 
 def load(path:Union[Path,str]) -> Tuple[List[pd.DataFrame], List[pd.DataFrame]]:
--- tests/test_dsads_loader.py
+++ tests/test_dsads_loader.py
@@ -7,7 +7,7 @@ import itertools
 from pathlib import Path
 
 sys.path.append('../src/')
-from sensorutils.datasets.dsads import load, load_raw, reformat
+from sensorutils.datasets.dsads import DSADS, load, load_raw, reformat
 
 
 class DSADSTest(unittest.TestCase):
@@ -21,8 +21,7 @@ class DSADSTest(unittest.TestCase):
             raise RuntimeError('dataset path is not specified')
 
     def setUp(self):
-        # self.loader = DSADS(self.path)
-        pass
+        self.loader = DSADS(self.path)
 
     def tearDown(self):
         pass
@@ -161,6 +160,66 @@ class DSADSTest(unittest.TestCase):
             d.dtypes[c] == np.dtype(np.float64) \
             for d in data for c in data[0].columns
         ]))
+    
+    def test_dsads_load_method_filed_labels(self):
+        # check processing for exceptions
+        x_labels = [
+            'T_xacc', 'T_yacc', 'T_zacc',
+            'T_xgyro', 'T_ygyro', 'T_zgyro',
+            'T_xmag', 'T_ymag', 'T_zmag',
+            'RA_xacc', 'RA_yacc', 'RA_zacc',
+            'RA_xgyro', 'RA_ygyro', 'RA_zgyro',
+            'RA_xmag', 'RA_ymag', 'RA_zmag',
+            'LA_xacc', 'LA_yacc', 'LA_zacc',
+            'LA_xgyro', 'LA_ygyro', 'LA_zgyro',
+            'LA_xmag', 'LA_ymag', 'LA_zmag',
+            'RL_xacc', 'RL_yacc', 'RL_zacc',
+            'RL_xgyro', 'RL_ygyro', 'RL_zgyro',
+            'RL_xmag', 'RL_ymag', 'RL_zmag',
+            'LL_xacc', 'LL_yacc', 'LL_zacc',
+            'LL_xgyro', 'LL_ygyro', 'LL_zgyro',
+            'LL_xmag', 'LL_ymag', 'LL_zmag'
+        ]
+        y_labels = ['activity', 'subject']
+
+        with self.assertRaises(ValueError):
+            _, _ = self.loader.load(subjects=None, x_labels=y_labels)
+
+        with self.assertRaises(ValueError):
+            _, _ = self.loader.load(subjects=None, y_labels=x_labels)
+
+        # shape check
+        ## x
+        for n in range(10, len(x_labels), 10):
+            x, _ = self.loader.load(subjects=None, x_labels=np.random.choice(x_labels, n, replace=False).tolist())
+            self.assertTupleEqual(x.shape[1:], (n, 125))
+        ## y
+        _, y = self.loader.load(subjects=None, y_labels=['activity'])
+        self.assertEqual(y.shape[1], 1)
+        _, y = self.loader.load(subjects=None, y_labels=['subject'])
+        self.assertEqual(y.shape[1], 1)
+        _, y = self.loader.load(subjects=None, y_labels=['activity', 'subject'])
+        self.assertEqual(y.shape[1], 2)
+
+    def test_dsads_load_method_filed_subjects(self):
+        patterns = [
+            [6, 2],
+            [4, 3, 1, 2, 5],
+            [5, 6, 4, 2],
+            [8, 3, 4, 2, 1],
+            [2, 5, 1, 7, 8, 4, 6, 3],
+            [8],
+            [5, 7, 2, 1, 8],
+            [4],
+            [8, 3, 6, 2, 1, 7, 5, 4],
+            [2, 7, 1, 8, 4, 6, 5],
+        ]
+
+        for i, subjects in enumerate(patterns):
+            print(f'pattern {i}')
+            with self.subTest(f'pattern {i}'):
+                _, y = self.loader.load(subjects=subjects)
+                self.assertSetEqual(set(np.unique(y[:, 1])), set(subjects))
 
 
 if __name__ == '__main__':
